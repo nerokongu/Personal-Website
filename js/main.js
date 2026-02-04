8b62060132647747b16fa5d0838e0df2683e565f
Quan <quankiu02@gmail.com>
2026-02-04 19:48:32 +0700

    check mobile 2

--- js/main.js
+++ js/main.js
@@ -453,12 +453,7 @@ contactPopup.addEventListener("click", (e) => {
   e.stopPropagation();
 });
 
-if (window.innerWidth <= 768) {
-  const mobileScreen = document.getElementById("mobile-only");
-  if (mobileScreen) {
-    mobileScreen.style.display = "flex";
-  }
-}
+
 
 
 
